3212ea8bf29140826d181a236adefbb72744e5c0
OmegaRed767 <omegared767@gmail.com>
2022-05-31 00:23:08 +0530

    Revert "police_branch"
    This reverts commit 489a8aae4c0367606d5681a3437d98aeccaf1c83.

--- Stack.js
+++ Stack.js
@@ -5,7 +5,6 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import Home from './Home';
 import Setting from './Setting';
-import Police from './Police';
 import {NavigationContainer} from '@react-navigation/native';
 const Stack = createStackNavigator();
 
@@ -15,7 +14,6 @@ const MyStack = () => {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Setting" component={Setting} />
-        <Stack.Screen name="Police" component={Police} />
       </Stack.Navigator>
     </NavigationContainer>
   );
