23211f59c3f7e7a6ab50bce90e3518b75a283162
OmegaRed767 <omegared767@gmail.com>
2022-06-01 23:55:44 +0530

    comment

--- Setting.js
+++ Setting.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import {Text, View, Button} from 'react-native';
+//functional components
+//another comment
+//last comment
 const Setting = ({navigation, route}) => {
   return (
     <View>
